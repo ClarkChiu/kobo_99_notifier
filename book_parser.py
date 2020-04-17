2e643af1b11790c826dd17ff66a6d89da0a4a4a9
Clark Chiu <freebooters@gmail.com>
2020-04-17 23:24:15 +0800

    - Add new branch and update/add workflows - Update the README - Update the config of telegram-send - Update the content of notification

--- book_parser.py
+++ book_parser.py
@@ -25,16 +25,12 @@ try:
             f'chat_id = {os.getenv("TELEGRAM_TO")}'
         )
 
-    if book["Coupon"]:
-        coupon_message = f'折扣碼： {book["Coupon"]}'
-    else:
-        coupon_message = f'不需折扣碼'
+    coupon_message = f'{book["Coupon"]}' if book["Coupon"] else '不需折扣碼'
 
     message = (
         f'書名： {book["Name"]}\n'
         f'作者： {book["Author"]}\n'
         f'出版社： {book["Publisher"]}\n'
-        f'{coupon_message}\n'
         f'簡介： {book["Intro"]}\n'
         f'[購買連結]({book["URL"]})'
     )
@@ -43,18 +39,25 @@ try:
     url = book['Image']
     file_name = os.path.basename(url)
     r = requests.get(url, stream=True)
+
     if r.status_code == 200:
         with open(file_name, 'wb') as book_cover:
             r.raw.decode_content = True
             shutil.copyfileobj(r.raw, book_cover) 
 
+    telegram_send.send(
+        messages=[message],
+        parse_mode='markdown',
+        conf='telegram-send.conf',
+    )
+
     with open(file_name, "rb") as book_cover:
         telegram_send.send(
-            messages=[message],
+            messages=[f'{coupon_message}'],
             images=[book_cover],
-            parse_mode='markdown',
             conf='telegram-send.conf',
         )
+
 except Exception as e:
     raise e
 
