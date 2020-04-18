84d147895893bdfd3f4e0e627ee10e1d1d072866
Clark Chiu <freebooters@gmail.com>
2020-04-18 16:06:42 +0800

    - Update the schedule in notification test for testing - Update the README - Add the time zone concept for book parser - Update the notification content and remove the book cover sending - Add pytz package to requirements.txt

--- book_parser.py
+++ book_parser.py
@@ -1,22 +1,27 @@
 import os
 import json
+import pytz
 import glob
 import datetime
 import telegram_send
-import requests
-import shutil
 
+# Get the latest book list on book_list folder
 list_of_files = glob.glob('./book_list/*')
 latest_file = max(list_of_files)
 
-with open(latest_file, 'r', encoding='utf-8') as f:
-    book_list = json.loads(f.readline())
+try:
+    with open(latest_file, 'r', encoding='utf-8') as f:
+        book_list = json.loads(f.readline())
+except Exception as e:
+    raise e
 
-idx = (datetime.datetime.today().weekday() + 4) % 7
+taiwan_tz = pytz.timezone('Asia/Taipei')
+taiwan_tz_today_weekday = (
+    datetime.datetime.today().astimezone(taiwan_tz).weekday()
+)
+idx = (taiwan_tz_today_weekday + 4) % 7
 
 try:
-    book = book_list[idx]
-
     # Create the configuration file for telegram
     with open('telegram-send.conf', 'w') as conf_file:
         conf_file.write(
@@ -25,39 +30,20 @@ try:
             f'chat_id = {os.getenv("TELEGRAM_TO")}'
         )
 
+    book = book_list[idx]
     coupon_message = f'{book["Coupon"]}' if book["Coupon"] else '不需折扣碼'
-
     message = (
-        f'書名： {book["Name"]}\n'
-        f'作者： {book["Author"]}\n'
-        f'出版社： {book["Publisher"]}\n'
-        f'簡介： {book["Intro"]}\n'
+        f'書名：{book["Name"]}\n'
+        f'作者：{book["Author"]}\n'
+        f'出版社：{book["Publisher"]}\n\n'
+        f'簡介：\n{book["Intro"]}\n'
         f'[購買連結]({book["URL"]})'
     )
 
-    # Download and sending the book cover
-    url = book['Image']
-    file_name = os.path.basename(url)
-    r = requests.get(url, stream=True)
-
-    if r.status_code == 200:
-        with open(file_name, 'wb') as book_cover:
-            r.raw.decode_content = True
-            shutil.copyfileobj(r.raw, book_cover) 
-
     telegram_send.send(
-        messages=[message],
+        messages=[message, f'{coupon_message}'],
         parse_mode='markdown',
         conf='telegram-send.conf',
     )
-
-    with open(file_name, "rb") as book_cover:
-        telegram_send.send(
-            messages=[f'{coupon_message}'],
-            images=[book_cover],
-            conf='telegram-send.conf',
-        )
-
 except Exception as e:
     raise e
-
