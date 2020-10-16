cf2470a97f38e52eca9783326d141c45cf247fb2
Clark Chiu <freebooters@gmail.com>
2020-10-16 21:44:42 +0800

    Remove some try/excpet blocks in core func

--- kobo_notifier/BasicFuncs.py
+++ kobo_notifier/BasicFuncs.py
@@ -48,80 +48,74 @@ class BasicFuncs(object):
         url = f'https://www.kobo.com/tw/zh/p/tw-dailydeal-bestofmonth'
 
         # Parser for kobo 99 event page
-        try:
-            kobo99_flow = requests.get(url=url, headers=self.headers)
-            kobo99_page = BeautifulSoup(kobo99_flow.content, 'html.parser')
-            today_99_block = kobo99_page.find('div', class_='SpotlightWidget')
+        kobo99_flow = requests.get(url=url, headers=self.headers)
+        kobo99_page = BeautifulSoup(kobo99_flow.content, 'html.parser')
+        today_99_block = kobo99_page.find('div', class_='SpotlightWidget')
 
-            coupon = re.search(r'(kobo\d{6})', today_99_block.text)
-            if coupon:
-                book_info['Coupon'] = coupon.group(1)
-            else:
-                book_info['Coupon'] = ''
+        coupon = re.search(r'(kobo\d{6})', today_99_block.text)
+        if coupon:
+            book_info['Coupon'] = coupon.group(1)
+        else:
+            book_info['Coupon'] = ''
 
-            book_info['URL'] = (
-                f"{self.age_verify_url}{today_99_block.find('a')['href']}"
-            )
-        except Exception as e:
-            raise e
+        book_info['URL'] = (
+            f"{self.age_verify_url}{today_99_block.find('a')['href']}"
+        )
 
         # Parser for book page
-        try:
-            book_page_flow = requests.get(
-                url=book_info['URL'], headers=self.headers
+        book_page_flow = requests.get(
+            url=book_info['URL'], headers=self.headers
+        )
+
+        book_page = BeautifulSoup(book_page_flow.content, 'html.parser')
+
+        resources_path = {
+            'Name': ['span', 'title product-field'],
+            'Author': ['a', 'contributor-name'],
+        }
+
+        for element in resources_path:
+            element_content = book_page.find(
+                resources_path[element][0],
+                class_=resources_path[element][1]
             )
 
-            book_page = BeautifulSoup(book_page_flow.content, 'html.parser')
-
-            resources_path = {
-                'Name': ['span', 'title product-field'],
-                'Author': ['a', 'contributor-name'],
-            }
-
-            for element in resources_path:
-                element_content = book_page.find(
-                    resources_path[element][0],
-                    class_=resources_path[element][1]
-                )
-
-                if element_content:
-                    book_info[element] = \
-                        list(element_content.stripped_strings)[0]
-                else:
-                    book_info[element] = 'N/A'
-
-            book_info['Intro'] = unicodedata.normalize(
-                'NFKD',
-                book_page.find(
-                    'div', class_='synopsis-description'
-                ).text
-            )
-
-            # Parser for book publication info
-            book_detail_info = book_page.find(
-                'div', class_='bookitem-secondary-metadata'
-            )
-
-            publisher_block = book_detail_info.find(
-                'a', class_='description-anchor'
-            )
-
-            book_info['Publisher'] = \
-                publisher_block.text if publisher_block else 'N/A'
-
-            publish_date_regex = re.search(
-                r'(\d{4}年\d{1,2}月\d{1,2}日)', book_detail_info.text
-            )
-
-            if publish_date_regex:
-                publish_date = datetime.strptime(
-                    publish_date_regex.group(1), '%Y年%m月%d日'
-                )
-                book_info['PublishDate'] = publish_date.strftime('%Y/%m/%d')
+            if element_content:
+                book_info[element] = \
+                    list(element_content.stripped_strings)[0]
             else:
-                book_info['PublishDate'] = 'N/A'
-        except Exception as e:
-            raise e
+                book_info[element] = 'N/A'
+
+        book_info['Intro'] = unicodedata.normalize(
+            'NFKD',
+            book_page.find(
+                'div', class_='synopsis-description'
+            ).text
+        )
+
+        # Parser for book publication info
+        book_detail_info = book_page.find(
+            'div', class_='bookitem-secondary-metadata'
+        )
+
+        publisher_block = book_detail_info.find(
+            'a', class_='description-anchor'
+        )
+
+        book_info['Publisher'] = \
+            publisher_block.text if publisher_block else 'N/A'
+
+        publish_date_regex = re.search(
+            r'(\d{4}年\d{1,2}月\d{1,2}日)', book_detail_info.text
+        )
+
+        if publish_date_regex:
+            publish_date = datetime.strptime(
+                publish_date_regex.group(1), '%Y年%m月%d日'
+            )
+            book_info['PublishDate'] = publish_date.strftime('%Y/%m/%d')
+        else:
+            book_info['PublishDate'] = 'N/A'
 
         return book_info
 
@@ -130,14 +124,11 @@ class BasicFuncs(object):
         url = f'{base_url}{self.latest_monday_date}'
 
         # Parser for event page
-        try:
-            kobo_event_flow = requests.get(url=url, headers=self.headers)
-            kobo_event_page = BeautifulSoup(
-                kobo_event_flow.content, 'html.parser'
-            )
-            title = kobo_event_page.find('h1', class_='article-title')
-        except Exception as e:
-            raise e
+        kobo_event_flow = requests.get(url=url, headers=self.headers)
+        kobo_event_page = BeautifulSoup(
+            kobo_event_flow.content, 'html.parser'
+        )
+        title = kobo_event_page.find('h1', class_='article-title')
 
         if title is not None and re.match(r'.+本週精選書單.+', title.text):
             books = kobo_event_page.find_all(
@@ -173,41 +164,32 @@ class BasicFuncs(object):
             return event_info
 
     def create_telegram_send_conf(self):
-        try:
-            telegram_token = os.getenv('TELEGRAM_TOKEN')
-            telegram_to = os.getenv('TELEGRAM_TO')
+        telegram_token = os.getenv('TELEGRAM_TOKEN')
+        telegram_to = os.getenv('TELEGRAM_TO')
 
-            if not telegram_token or not telegram_to:
-                raise Exception(
-                    'The telegram-send related info are not in env variable'
-                )
+        if not telegram_token or not telegram_to:
+            raise Exception(
+                'The telegram-send related info are not in env variable'
+            )
 
-            with open('telegram-send.conf', 'w') as conf_file:
-                conf_file.write(
-                    '[telegram]\n'
-                    f'token = {telegram_token}\n'
-                    f'chat_id = {telegram_to}'
-                )
-        except Exception as e:
-            raise e
+        with open('telegram-send.conf', 'w') as conf_file:
+            conf_file.write(
+                '[telegram]\n'
+                f'token = {telegram_token}\n'
+                f'chat_id = {telegram_to}'
+            )
 
     def send_notification(self, message_list):
-        try:
-            send(
-                messages=message_list,
-                parse_mode='text',
-                conf='telegram-send.conf',
-            )
-        except Exception as e:
-            raise e
+        send(
+            messages=message_list,
+            parse_mode='text',
+            conf='telegram-send.conf',
+        )
 
     def create_checkpoint(self):
-        try:
-            os.makedirs(
-                os.path.dirname(self.checkpoint_filepath), exist_ok=True
-            )
+        os.makedirs(
+            os.path.dirname(self.checkpoint_filepath), exist_ok=True
+        )
 
-            with open(self.checkpoint_filepath, 'w') as check_point_file:
-                pass
-        except Exception as e:
-            raise e
+        with open(self.checkpoint_filepath, 'w') as check_point_file:
+            pass
